3cd52dbfdbd145ddd01b3fa091ec811fbcc55519
Eduardo Aviles <eduardoa@ssnapp.com>
2018-06-14 01:43:54 -0600

    fixing docker file

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,7 +3,7 @@ python3 manage.py makemigrations api_auth predictor
 python3 manage.py migrate
 python3 manage.py collectstatic  # Collect static files
 
-supervisord -c /etc/supervisor/conf.d/supervisor-app.conf
+supervisord -n
 
 #certbot --nginx --non-interactive --agree-tos -m eduardo.avilesj@gmail.com -d a.oraculapp.com
 
