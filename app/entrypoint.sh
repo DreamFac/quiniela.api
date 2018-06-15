5ac1f4f3006e2989e9000993bad54bfb3b09ef97
steelcolosus <eduardo.avilesj@gmail.com>
2018-06-15 17:35:31 +0000

    ssl automation

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -3,7 +3,7 @@ python3 manage.py makemigrations api_auth predictor
 python3 manage.py migrate
 python3 manage.py collectstatic  # Collect static files
 
-supervisord -n
+supervisord
 
 certbot --nginx --non-interactive --agree-tos -m eduardo.avilesj@gmail.com -d a.oraculapp.com
 
